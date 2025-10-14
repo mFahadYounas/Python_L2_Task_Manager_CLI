6dbf4b3dfb36b577e75546349d7da9bc75c42a78
mFahadYounas <fahadyounas97@gmail.com>
2025-10-14 11:26:47 +0500

    Fixed bug where lack of storage directory causes error

--- cli/store.py
+++ cli/store.py
@@ -1,6 +1,19 @@
 import json
+import os
 
 
 def create_store() -> None:
+    directory_name = "storage"
+
+    try:
+        os.mkdir(directory_name)
+        print(f"Directory '{directory_name}' created successfully.")
+    except FileExistsError:
+        print(f"Directory '{directory_name}' already exists.")
+    except PermissionError:
+        print(f"Permission denied: Unable to create '{directory_name}'.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
     with open("storage/store.json", "w") as jf:
         json.dump([], jf)
